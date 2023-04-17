54c48f3ddb452b8f6bf3aea9a9c44d86dbe9f42e
Clauss Clement <clement_clauss@orange.fr>
2023-04-17 12:16:16 +0200

    WIP Ajout system de vague

---
+++ Assets/script/WaveSpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Transform mobPrefab;
+    [SerializeField]
+    private Transform spawnPoint;
+    [SerializeField]
+    private float timeBetweenWaves = 5f;
+
+    private float countdown = 2f;
+
+    private int waveIndex = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countdown <= 0f)
+        {
+            // Une coroutine s'apelle de cette manière
+            // StartCoroutine(nomCoroutine);
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+    // Une coroutine permet d'avoir un délai entre 2 apelles, besoin d'un retour
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnMob();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        
+    }
+
+    void SpawnMob()
+    {
+        Instantiate(mobPrefab,spawnPoint.position   ,spawnPoint.rotation);
+    }
+}
