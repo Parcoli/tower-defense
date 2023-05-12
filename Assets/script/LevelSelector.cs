cad25ab01638ef5cf71e08849320fc20522e274c
Clauss Clement <clement_clauss@orange.fr>
2023-05-12 20:13:31 +0200

    Ajout du Choix des levels

---
+++ Assets/script/LevelSelector.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public SceneFader fader;
+
+    public void Select(string levelName)
+    {
+        fader.FadeTo(levelName);
+    }
+}
